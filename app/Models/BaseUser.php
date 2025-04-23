dad6908ecf124ccae2b2d38768880dfdce2224a7
marco76tv <marco.sottana@gmail.com>
2025-04-23 15:42:01 +0200

    .

--- app/Models/BaseUser.php
+++ app/Models/BaseUser.php
@@ -413,11 +413,9 @@ abstract class BaseUser extends Authenticatable implements HasName, HasTenants, 
      */
     /**
      * Get all role names associated with the user.
-<<<<<<< HEAD
+
      * 
-=======
-     *
->>>>>>> aurmich/dev
+aurmich/dev
      * @return array<int, string>
      */
     public function getRoleNames(): array
@@ -559,21 +557,15 @@ abstract class BaseUser extends Authenticatable implements HasName, HasTenants, 
         return $this->belongsTo(Team::class, 'current_team_id');
     }
 
-<<<<<<< HEAD
-=======
-    /*
-     * Moved to HasTenants trait
-     *
->>>>>>> aurmich/dev
+
+aurmich/dev
     public function tenants(): BelongsToMany
     {
         return $this->belongsToMany(Tenant::class, 'tenant_user');
     }
-<<<<<<< HEAD
 
-=======
-    */
->>>>>>> aurmich/dev
+
+aurmich/dev
     public function authentications(): MorphMany
     {
         return $this->morphMany(\Modules\User\Models\Authentication::class, 'authenticatable');
