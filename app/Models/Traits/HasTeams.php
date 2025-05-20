19756dad5c5f36be73e72b01b2a1c922991677f3
marco76tv <marco.sottana@gmail.com>
2025-05-20 14:10:46 +0200

    .

--- app/Models/Traits/HasTeams.php
+++ app/Models/Traits/HasTeams.php
@@ -68,21 +68,8 @@ trait HasTeams
     /**
      * Check if the user belongs to a specific team.
      */
-<<<<<<< HEAD
     public function belongsToTeam(\Modules\User\Contracts\TeamContract $team): bool
     {
-=======
-<<<<<<< HEAD
-    public function belongsToTeam(?TeamContract $team): bool
-    {
-        if ($team === null) {
-            return false;
-        }
-=======
-    public function belongsToTeam(\Modules\User\Contracts\TeamContract $team): bool
-    {
->>>>>>> da3d3a0 (.)
->>>>>>> aurmich/dev
         $found = $this->teams()->where('teams.id', $team->id)->first();
         if ($found === null) {
             return false;
@@ -269,15 +256,7 @@ trait HasTeams
     {
         $xot = XotData::make();
         $teamClass = $xot->getTeamClass();
-<<<<<<< HEAD
-        return $this->hasMany($teamClass, 'user_id'); 
-=======
-<<<<<<< HEAD
         return $this->hasMany($teamClass, 'user_id');
-=======
-        return $this->hasMany($teamClass, 'user_id'); 
->>>>>>> da3d3a0 (.)
->>>>>>> aurmich/dev
     }
 
     /**
@@ -372,15 +351,7 @@ trait HasTeams
      *
      * @param \Modules\User\Contracts\TeamContract $team
      */
-<<<<<<< HEAD
     public function switchTeam(\Modules\User\Contracts\TeamContract $team): bool
-=======
-<<<<<<< HEAD
-    public function switchTeam(?TeamContract $team): bool
-=======
-    public function switchTeam(\Modules\User\Contracts\TeamContract $team): bool
->>>>>>> da3d3a0 (.)
->>>>>>> aurmich/dev
     {
         if (! $this->belongsToTeam($team)) {
             return false;
