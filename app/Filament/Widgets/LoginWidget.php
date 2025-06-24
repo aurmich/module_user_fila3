4ce9181481cc84a0dd6f00db2c09ce561b2309e6
marco76tv <marco.sottana@gmail.com>
2025-06-24 18:28:00 +0200

    .

--- app/Filament/Widgets/LoginWidget.php
+++ app/Filament/Widgets/LoginWidget.php
@@ -35,25 +35,8 @@ class LoginWidget extends XotBaseWidget
      */
     protected static string $view = 'user::filament.widgets.login';
     
-<<<<<<< HEAD
    
-=======
-    /** @var int|string|array<string, mixed> */
-    protected int | string | array $columnSpan = 'full';
-    
-    /**
-     * Dati del form per il login
-     *
-     * @var array<string, mixed>|null
-     */
-    public ?array $data = [];
-
->>>>>>> cb31b56 (.)
-    /**
-     * @var \Filament\Forms\Form
-     */
-    public ?\Filament\Forms\Form $form = null;
-
+   
     /**
      * Inizializza il widget quando viene montato.
      *
@@ -61,7 +44,7 @@ class LoginWidget extends XotBaseWidget
      */
     public function mount(): void
     {
-        $this->form = $this->makeForm();
+        //$this->form = $this->makeForm();
         $this->form->fill();
     }
     
@@ -80,12 +63,7 @@ class LoginWidget extends XotBaseWidget
             TextInput::make('password')
                 ->password()
                 ->required(),
-<<<<<<< HEAD
             Toggle::make('remember'),
-=======
-            Toggle::make('remember')
-                ->label(__('user::auth.remember_me')),
->>>>>>> cb31b56 (.)
         ];
     }
 
@@ -154,11 +132,7 @@ class LoginWidget extends XotBaseWidget
                 
             $this->form->fill();
             $this->form->saveRelationships();
-<<<<<<< HEAD
             //$this->form->callAfter();
-=======
-            $this->form->callAfter();
->>>>>>> cb31b56 (.)
             
             foreach ($e->errors() as $field => $messages) {
                 $this->form->getComponent($field)?->getContainer()->getParentComponent()?->getStatePath()
